37f3f7a308bfbbc5c662bf5a2c4e18aaad55ea82
mrtripop <tripop.tor@qualint.io>
2023-03-25 14:49:42 +0700

    screen section

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,14 @@
 import '../src/index.css';
 
+// Registers the msw addon
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
+// Initialize MSW
+initialize();
+
+// Provide the MSW addon decorator globally
+export const decorators = [mswDecorator];
+
 export const parameters = {
 	// parameters use to control behavior or Storybook features
 	actions: { argTypesRegex: '^on[A-Z].*' }, // mock callbacks
